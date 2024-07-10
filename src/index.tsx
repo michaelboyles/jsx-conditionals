3ee0a5c6f54c0aa04a3e8b80029b02e87309c14f
Michael Boyles <michael@boyl.es>
2024-07-10 10:42:04 +0100

    fix: truncated error message

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const warning = " is a special component which is expected to be removed by transform at compile-time. If you're"
-     "seeing this message then the transform wasn't successful";
+     + " seeing this message then the transform wasn't successful";
 
 export interface IfProps {
     condition: any;
